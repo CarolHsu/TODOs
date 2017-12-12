2d28b1308910ef2b03ee88791ebaefc98ad7fdbe
shesee <sheseee@gmail.com>
2017-12-12 22:26:48 +0800

    Modify routes' names and trying to add google canlendar APIs

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,47 +1,118 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Task = mongoose.model('Tasks');
+var fs = require('fs'),
+    readline = require('readline'),
+    google = require('googleapis'),
+    googleAuth = require('google-auth-library');
 
-exports.list_all_tasks = function (req, res) {
-    Task.find({}, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl)
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from the page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token,', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to: ', TOKEN_PATH);
+}
+
+// API major part
+function listEvents(auth) {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResult: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returns an error: ', err)
+            return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming 10 events');
+            for (var i = 0, len = events.length; i < len; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                console.log('%s - %s', start, event.summary);
+            }
+        }
+    });
+}
+
+exports.index = function (req, res) {
+    fs.readFile('client_secret.json', function processClientSecrets (err, content) {
+        if (err) {
+            console.log('Get error when loading client secret file: ' + err);
+            return;
+        }
+        authorize(JSON.parse(content), listEvents);
     });
 };
 
-exports.create_a_task = function (req, res) {
-    var new_task = new Task(req.body);
-    new_task.save(function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
+exports.create = function (req, res) {
 };
 
-exports.read_a_task = function (req, res) {
-    Task.findById(req.params.taskId, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
+exports.show = function (req, res) {
 };
 
-exports.update_a_task = function (req, res) {
-    Task.findOneAndUpdate(req.params.taskId, req.body, { new: true }, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
+exports.update = function (req, res) {
 };
 
-exports.delete_a_task = function (req, res) {
-    Task.remove({
-        _id: req.params.taskId
-    }, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json({message: 'Task successfully deleted!'});
-    });
+exports.destroy = function (req, res) {
 };
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
     var todoList = require('../controllers/todoListController');
 
     app.route('/tasks')
-    .get(todoList.list_all_tasks)
-    .post(todoList.create_a_task);
+    .get(todoList.index)
+    .post(todoList.create);
 
     app.route('/tasks/:taskId')
-    .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task)
+    .get(todoList.show)
+    .put(todoList.update)
+    .delete(todoList.destroy)
 };
